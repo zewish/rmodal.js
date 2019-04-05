f7975381442c50224827872c60daf5808193d5ca
Emil Hemdal <emil@hemdal.se>
2019-04-05 10:01:25 +0200

    Add TypeScript definition file

---
+++ index.d.ts
@@ -0,0 +1,35 @@
+// Type definitions for rmodal.js 1.0.31
+// Project: rmodal.js
+// Definitions by: Emil Hemdal https://emil.hemdal.se/
+
+export = RModal;
+
+declare class RModal {
+  constructor(el: HTMLElement, opts?: RModalOptions);
+
+  version: string;
+
+  close(): void;
+  content(content?: string): string | void;
+  elements(selector: string | string[], fallback?: boolean): Array<HTMLElement>;
+  focus(el?: HTMLElement): void;
+  keydown(ev: KeyboardEvent): void;
+  open(content: string): void;
+}
+
+declare namespace RModal {
+  export interface RModalOptions {
+    afterClose?(): void;
+    beforeOpen?(callback: Function): void;
+    bodyClass?: string;
+    closeTimeout?: number;
+    content?: string;
+    dialogClass?: string;
+    dialogOpenClass?: string;
+    dialogCloseClass?: string;
+    escapeClose?: boolean;
+    focus?: boolean;
+    focusElements?: string[];
+  }
+}
+
